73d709da958c3500edf15d2b697aa7bcfbaac555
Christian Mailind <chma54118@edu.ucl.dk>
2023-03-01 14:57:22 +0100

    Add project files.

---
+++ lommeregner/Calculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lommeregner
+{
+    public class Calculator
+    {
+        double num1;
+        double num2;
+        int userchoice;
+
+          public void usermenu()
+        {
+            Console.Clear();
+            Console.WriteLine("MENU: ");
+            Console.WriteLine("\n1. +  Add. \n2. - Subtract. \n3. * Multiply. \n4. /  Divide. \n5. Exit Program.");
+            Console.WriteLine("Choose.");
+        }
+
+
+        public void userinput()
+        {
+            try
+            {
+                Console.Write("\nType the first number: ");
+                num1 = double.Parse(Console.ReadLine());
+                Console.Write("\nType the second number: ");
+                num2 = double.Parse(Console.ReadLine());
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("\n Not a valid format.");
+                Console.WriteLine("\nPlease only use integers!");
+                userinput();
+            }
+        }
+        public void useroutput()
+        {
+            Console.WriteLine($"\n {userchoice} is not an option.");
+            Console.WriteLine("\nPress enter to go back to menu.");
+            Console.ReadLine();
+        }
+
+        public void add()
+        {
+            double result = num1 + num2;
+            Console.WriteLine($"\n the sum of {num1} and {num2} is {result}");
+            Console.Write("\n Press enter to go back to menu.");
+            Console.ReadLine();
+        }
+        public void subtract()
+        {
+            double result = num1 + num2;
+            Console.WriteLine($"\n the subtraction of {num1} and {num2} is {result}");
+            Console.Write("\n Press enter to go back to menu.");
+            Console.ReadLine();
+        }
+        public void multiply()
+        {
+            double result = num1 + num2;
+            Console.WriteLine($"\n the ¨multiplication of {num1} and {num2} is {result}");
+            Console.Write("\n Press enter to go back to menu.");
+            Console.ReadLine();
+        }
+        public void divide()
+        {
+            try
+            {
+                double result = num1 + num2;
+                Console.WriteLine($"\n the fraction of {num1} and {num2} is {result}");
+                Console.Write("\n Press enter to go back to menu.");
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("\ncannot divide by 0");
+                Console.WriteLine("\nChoose an integer other than 0");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ lommeregner/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace lommeregner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator user = new Calculator();
+            int userchoice = 0;
+
+            do
+            {
+                user.usermenu();
+                try
+                {
+                    userchoice = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    user.useroutput();
+                }
+
+                switch (userchoice)
+                {
+                    case 1:
+                        user.userinput();
+                        user.add();
+                        break;
+
+                    case 2:
+                        user.userinput();
+                        user.subtract();
+                        break;
+                    case 3:
+                        user.userinput();
+                        user.multiply();
+                        break;
+                    case 4:
+                        user.userinput();
+                        user.divide();
+                        break;
+                    case 5:
+                        break;
+
+                    default:
+
+                        user.useroutput();
+                        break;
+                }
+            }
+            while (userchoice != 5);
+        }   
+    }
+}
